7db5c6ea5be2e9c7aad9eee0bbe0d9e9a22769a9
huii-3 <hbgarcia@mi.unc.edu.ar>
2026-08-24 00:18:42 -0300

    ej tarea actualizados

--- tarea1/ej1tarea.m
+++ tarea1/ej1tarea.m
@@ -1,6 +1,8 @@
 clear all, close all, clc
 
 pkg load symbolic
+pkg load control
+
 
 syms G1 G2 G3 G4 H1 H2 H3 real
 
@@ -28,4 +30,32 @@ G7eq = G6eq/(1+G6eq*H3)
 %simplifico la funcion de transferencia
 
 G7final = simplify(G7eq)
+
+%simulacion
+
+%valroes
+G1_v = 2
+G2_v = 3
+G3_v = 4
+G4_v = 5
+H1_v = 1
+H2_v = 1
+H3_v = 1
+
+%sustituyo valores
+
+G7_sust = subs(G7final, {G1, G2, G3, G4, H1, H2, H3}, {G1_v, G2_v, G3_v, G4_v, H1_v, H2_v, H3_v})
+
+%obtener G7_sust como numero
+G7_sust = double(G7_sust)
+
+
+
+
+
+
+
+
+
+
 
--- tarea1/ej2tarea.m
+++ tarea1/ej2tarea.m
@@ -19,5 +19,21 @@ St = S1+S2
 %simplifico
 ST = simplify(St)
 
+%simulacion
+
+%asigno valores
+
+G1_v = 1
+G2_v = 1
+K1_v = 4
+K2_v = 2
+
+%sustituyo
+
+ST_sust = subs(ST,{G1, G2, K1, K2},{G1_v, G2_v, K1_v, K2_v})
+
+ST_sust = double(ST_sust)
+
+
 
 
