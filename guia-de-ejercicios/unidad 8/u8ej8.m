66a846b65b2c338d0581e7a0ca147996c0a1b6f9
huii-3 <hbgarcia@mi.unc.edu.ar>
2026-09-11 20:31:13 -0300

    agrego algunos apartados

--- guia-de-ejercicios/unidad 8/u8ej8.m
+++ guia-de-ejercicios/unidad 8/u8ej8.m
@@ -1,50 +1,102 @@
-close all, clear all, clc
+% UNIDAD 8 - EJERCICIO 8
 
-pkg load control
-pkg load symbolic
+% Esbozar la respuesta temporal de los sistemas caracterizados
+% por las siguientes funciones de transferencia.
+
+%.....................................................................
+% Ejercicio 8.1
+
+close all, clear all, clc;
+
+pkg load control;
+
+%defino la fdt
+
+s = tf('s');
+G1 = 15/(5*s+1)
+
+disp('polos del sistema original')
+pole(G1)
+step(G1)
+
+%.....................................................................
+% Ejercicio 8.6
+
+close all, clear all, clc;
+
+pkg load control;
+pkg load symbolic;
 
 
-s = tf('s')
+s = tf('s');
 G6 = 625/(s^2+60*s+625)
 disp('polos del sistema original')
 pole(G6)
-step(G6,0,4)
+step(G6);
 
 %Veo q el valor final es 1
-%y que es un sistema de 2do orden subamortiguado
-
- close all, clear all, clc
-
- pkg loas symbolic
- pkg load control
-
- %s= tf('s')
- %g10 = 187500/(s^2 + 600*s + 62500)
-
- %step(G10)
+%y que es un sistema de 2do orden sobreamortiguado
 
 
-%G = 625/(s^2)
+%.....................................................................
+% Ejercicio 8.12
+
+close all, clear all, clc
+
+pkg load symbolic
+pkg load control
+
+s= tf('s')
+G12 = 0.1875/(s^2 + 0.35*s + 0.0625)
+
+disp('polos del sistema original')
+pole(G12)
+step(G12)
+
+%veo que es un sistema subamortiguado, ya que tiene un sobrepasamiento
+% se amortigua y llega a un valor final cte
+
+%.....................................................................
+% Ejercicio 8.16
+
+close all, clear all, clc
+
+pkg load symbolic
+pkg load control
+
+s= tf('s')
+G16 = 0.0625/(s^2 + 0.0625)
+
+disp('polos del sistema original')
+pole(G16)
+step(G16)
+
+% veo que es un sistema no amortiguado, ya que oscila
+% sin amortiguarse nunca (amplitud constante)
+
+%.....................................................................
+% Ejercicio 8.19
+
+close all, clear all, clc
+
+pkg load symbolic
+pkg load control
+
+s= tf('s')
+G19 = 2/((3*s+1)^2)
+
+disp('polos del sistema original')
+pole(G19)
+step(G19)
+
+%veo que es un sistema sobreamortiguado ya que tiene un punto
+%de inflexion (cambia la concavidad), y no hay sobrepasamiento.
 
 
-syms psita
-
-%veo que el valor final es 3 y q es un sist sde 2orden ubamortiguado
-K = 3
-tp = 0.015
-Mp = 100*(3.25-3)/3
 
 
-%aplico formulas  para calcular parametros del sist de segundo orden
-psita = log(Mp)/(sqrt(pi^2)+ (log(Mp))^2)
-wn=pi/(tp*sqrt(1-psita^2))
-
-%armo la funcion de transferencia
-s=tf('s')
-G2 = ()
 
 
-%eq = exp((-pi*psita)/(sqrt(1-psita^2))) == Mp
-%psita = double(solve(eq,psita))
+
 
 
